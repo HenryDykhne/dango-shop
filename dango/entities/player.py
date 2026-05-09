ba6ed9489d39403550820b7a845ec1dc417eec42
Henry <Hdykhne@hotmail.ca>
2026-05-09 19:09:25 -0400

    added wasabi and coal hazards. added endlag to parry and stab

--- dango/entities/player.py
+++ dango/entities/player.py
@@ -1,6 +1,7 @@
 import pygame
 from dango.settings import FIELD_TOP, FIELD_BOTTOM, PLAYER_SPEED
 from dango.entities.stick import Stick
+from dango.settings import FIELD_TOP, FIELD_BOTTOM, PLAYER_SPEED
 
 
 class Player:
@@ -11,7 +12,10 @@ class Player:
         self.h = 60
         self.speed = PLAYER_SPEED
         self.scramble_controls_till = pygame.time.get_ticks() / 1000.0 - 1.0  # time until which controls are scrambled; initialized to past time
+
         self.stick = Stick(add_stick, day)
+        self.speed = PLAYER_SPEED
+        self.scramble_controls_till = pygame.time.get_ticks() / 1000.0 - 1.0  # time until which controls are scrambled; initialized to past time
         # visual parry effects: list of dicts {dir, t, dur}
         self._parries = []
         # visual stab effects: list of dicts {t, dur}
